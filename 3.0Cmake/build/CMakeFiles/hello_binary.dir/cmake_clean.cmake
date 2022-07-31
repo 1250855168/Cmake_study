6b86c92d38b576d1b8d4713a829ee93099e72962
YANG <1250855168@qq.com>
2022-07-31 18:02:00 +0800

    updata

---
+++ 3.0Cmake/build/CMakeFiles/hello_binary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_binary.dir/main.c.o"
+  "hello_binary"
+  "hello_binary.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello_binary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 3.0Cmake/main.c
@@ -0,0 +1,7 @@
+#include "include/hello.h"
+ 
+ int main()
+ {
+  hello("Shared Libaray!");
+  return 0;
+ }
