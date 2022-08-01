11f4f45575b7d8b24ae244d5a7381dda97287777
YANG <1250855168@qq.com>
2022-08-01 20:39:09 +0800

    1

---
+++ 4.0Cmake/build/CMakeFiles/cmake_build.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_build.dir/main.c.obj"
+  "CMakeFiles/cmake_build.dir/src/hello.c.obj"
+  "cmake_build.pdb"
+  "cmake_build.exe"
+  "cmake_build.exe.manifest"
+  "libcmake_build.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmake_build.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
